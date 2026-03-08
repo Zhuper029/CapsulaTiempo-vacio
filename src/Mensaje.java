c5d9e9c6b31c1dff93927f9d507190df56cd6fec
Wenhao <Wenhao@DESKTOP-4H579DF>
2026-03-08 22:21:52 +0100

    "Versión 1.0: Segundo Lanzamiento"

--- src/Mensaje.java
+++ src/Mensaje.java
@@ -2,6 +2,8 @@
 public class Mensaje {
 	
 	public static void main(String[] args) {
-		System.out.println("Mensaje para el año 2026: ¡Git funciona!");
+		System.out.println("***********************************");
+		System.out.println("MENSAJE DEL FUTURO: ¡HOLA MUNDO!");
+		System.out.println("***********************************");
 	}
 }
